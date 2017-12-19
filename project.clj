9b5c5801f79c2ad45c8fdc7cb43d20ea3c8453bd
madvas <matus.lestan@gmail.com>
2017-12-19 09:08:13 +0100

    update config dep

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject district0x/district-server-smart-contracts "1.0.0"
+(defproject district0x/district-server-smart-contracts "1.0.1"
   :description "district0x server component for handling smart-contracts"
   :url "https://github.com/district0x/district-server-smart-contracts"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cljs-web3 "0.19.0-0-8"]
-                 [district0x/district-server-config "1.0.0"]
-                 [district0x/district-server-web3 "1.0.0"]
+                 [district0x/district-server-config "1.0.1"]
+                 [district0x/district-server-web3 "1.0.1"]
                  [mount "0.1.11"]
                  [org.clojure/clojurescript "1.9.946"]]
 
