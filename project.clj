994bbe9b4c716b4752dc4b6eed9fa6824ba169db
fbielejec <fbielejec@gmail.com>
2018-09-18 14:38:05 +0200

    fix tests

--- project.clj
+++ project.clj
@@ -13,10 +13,10 @@
   :plugins [[lein-npm "0.6.2"]
             [lein-doo "0.1.8"]
             [lein-solc "1.0.1-1"]]
-  
+
   :npm {:dependencies [[deasync "0.1.11"]]
         :devDependencies [[ws "2.0.1"]]}
-  
+
   :solc {:src-path "resources/public/contracts/src"
          :build-path "resources/public/contracts/build"
          :solc-err-only true
@@ -24,8 +24,13 @@
          :wc true
          :contracts :all}
 
+  :source-paths ["src"]
+
   :clean-targets ^{:protect false} ["target" "tests-compiled"]
-  
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]
+                   :resource-paths ["resources"]}}
+
   :cljsbuild {:builds [{:id "tests"
                         :source-paths ["src" "test"]
                         :compiler {:main "tests.runner"
