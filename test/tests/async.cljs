8af8a0ea9ebcb6fa3ead5404559dc9a5fce1b817
fbielejec <fbielejec@gmail.com>
2018-09-18 14:57:28 +0200

    share test suite between sync/async

--- test/tests/async.cljs
+++ test/tests/async.cljs
@@ -1,8 +1,8 @@
 (ns tests.async
-  (:require [cljs.test :refer-macros [deftest is testing use-fixtures]]
-            [district.server.smart-contracts :as contracts]
+  (:require [cljs.test :refer-macros [deftest use-fixtures]]
             [mount.core :as mount]
-            [tests.smart-contracts]))
+            [tests.smart-contracts]
+            [tests.suite :refer [test-smart-contracts]]))
 
 (use-fixtures
   :once
@@ -17,31 +17,4 @@
             (mount/stop))})
 
 (deftest test-smart-contracts-async
-  (is (false? (empty? (contracts/contract-abi :my-contract))))
-  (is (false? (empty? (contracts/contract-bin :my-contract))))
-  (is (= (contracts/contract-address :my-contract) "0x0000000000000000000000000000000000000000"))
-  (is (= (contracts/contract-name :my-contract) "MyContract"))
-
-  (is (map? (contracts/deploy-smart-contract! :my-contract {:arguments [1]})))
-
-  (is (not= (contracts/contract-address :my-contract) "0x0000000000000000000000000000000000000000"))
-
-  (is (= 1 (.toNumber (contracts/contract-call :my-contract :counter))))
-
-  (is (= 5 (.toNumber (contracts/contract-call :my-contract :my-plus 2 3))))
-
-  (let [tx-hash (contracts/contract-call :my-contract :increment-counter 2 {:gas 500000})]
-    (is (string? tx-hash))
-    (let [{:keys [:args]} (contracts/contract-event-in-tx tx-hash :my-contract :on-counter-incremented)]
-      (is (= 3 (.toNumber (:the-counter args))))))
-
-  (let [tx-hash (contracts/contract-call :my-contract :double-increment-counter 2 {:gas 500000})]
-    (is (string? tx-hash))
-    (is (= (map (comp #(.toNumber %) :the-counter :args)
-                (contracts/contract-events-in-tx tx-hash :my-contract :on-counter-incremented))
-           [5 7])))
-
-  ;; corner case, function which returns address
-  (is (= "0xbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef" (contracts/contract-call :my-contract :target)))
-
-  (is (= 7 (.toNumber (contracts/contract-call :my-contract :counter)))))
+  (test-smart-contracts))
