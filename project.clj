a001d27821f97e1db6eaf02f6b49588a9a5c55ce
filip <fbielejec@gmail.com>
2019-06-21 14:18:46 +0200

    async-helpers++

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cljs-web3 "0.19.0-0-8"]
-                 [district0x/async-helpers "0.1.0"]
+                 [district0x/async-helpers "0.1.1"]
                  [district0x/district-server-config "1.0.1"]
                  [district0x/district-server-web3 "1.0.1"]
                  [mount "0.1.11"]
