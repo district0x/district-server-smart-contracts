0f8b949b610c569c8b5dfbc6cb403d8fab2d2ec2
filip <fbielejec@gmail.com>
2020-02-11 18:23:15 +0100

    processign blocks in chunks of step-size

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject district0x/district-server-smart-contracts "1.2.3-SNAPSHOT"
+(defproject district0x/district-server-smart-contracts "1.2.4-SNAPSHOT"
   :description "district0x server module for handling smart-contracts"
   :url "https://github.com/district0x/district-server-smart-contracts"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljs-web3-next "0.1.1"]
+  :dependencies [[cljs-web3-next "0.1.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [district0x/async-helpers "0.1.3"]
                  [district0x/district-server-config "1.0.1"]
-                 [district0x/district-server-web3 "1.2.0"]
+                 [district0x/district-server-web3 "1.2.4"]
                  [mount "0.1.16"]
                  [org.clojure/clojurescript "1.10.520"]]
 
